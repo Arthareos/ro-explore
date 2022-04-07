982d74b8d8fe336daa0b2ddd0f35c0511a290d41
Simone Di Fonzo <difonzosimone@hotmail.com>
2022-04-07 12:38:04 +0300

    Fixed autoplay bug

--- Server/public/build/explore-ui/src/app/app.component.ts
+++ Server/public/build/explore-ui/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'explore-ui';
+
+  ngOnInit(): void {
+    var video = document.querySelector('video');
+    if (video) {
+      video.muted = true;
+      video.autoplay = true;
+      video.play();
+    }
+  }
 }
